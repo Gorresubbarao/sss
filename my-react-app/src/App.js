17bd7b3efe5ba24fa38dad75127e6ac8265fcf04
Gorresubbarao <gsubbu2109@gmail.com>
2024-11-20 14:10:59 +0530

    intail

---
+++ my-react-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Subbarao G
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
